e6b3b3997d39f9a78dd9ee638c6a115460bd578b
Haider Malik <haidermalik504@gmail.com>
2018-02-07 12:48:48 +0500

    create schema from models

---
+++ bin/automigrate.js
@@ -0,0 +1,16 @@
+/**
+ * Created by haidermalik504 on 2/7/18.
+ */
+'use strict';
+
+const app = require('../server/server');
+const ds = app.datasources.mysqlDs;
+
+ds.automigrate(['Customer'], (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('models created!');
+  ds.disconnect();
+  process.exit();
+});
