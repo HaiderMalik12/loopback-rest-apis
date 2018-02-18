aba6ee64d84161d914ed115590d1b0fc9432065f
Haider Malik <haidermalik504@gmail.com>
2018-02-18 14:08:51 +0500

    one to one as a separate table

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -6,7 +6,7 @@
 const app = require('../server/server');
 const ds = app.datasources.mysqlDs;
 
-ds.automigrate(['Customer','Order','OrderItem', 'Item'], (err) => {
+ds.automigrate(['Customer','Order','OrderItem', 'Item','CreditCard'], (err) => {
   if (err) {
     throw err;
   }
--- bin/autoupdate.js
+++ bin/autoupdate.js
@@ -3,7 +3,7 @@
 const app = require('../server/server');
 const ds = app.datasources.mysqlDs;
 
-ds.autoupdate(['Customer','Order','OrderItem', 'Item'], err => {
+ds.autoupdate(['Customer','Order','OrderItem', 'Item','CreditCard'], err => {
   if (err) throw err;
   console.log('models synced!');
   ds.disconnect();
